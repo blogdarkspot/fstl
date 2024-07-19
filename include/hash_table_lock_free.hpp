b2fb62332ccd6c55838cae47e62aa18696f4d217
Fábio da Silva Santana <fabio.ssantana.dev@gmail.com>
2024-07-19 18:45:45 -0300

    stack_list lock free

--- include/hash_table_lock_free.hpp
+++ include/hash_table_lock_free.hpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <list>
 #include <vector>
+#include <atomic>
 #include <iostream>
 
 template<typename NodeT>
@@ -14,44 +15,50 @@ struct iterator_list {
 
 	iterator_list() : _M_data(nullptr) {}
 
-	iterator_list(NodeT* data) : _M_data(data) {}
+	explicit iterator_list(NodeT* data)   : _M_data(data) {}
 
 	reference operator*() {
 		return _M_data->_M_value;
 	}
 
-	iterator_list& operator--()
+	iterator_list<NodeT>& operator--()
 	{
 		_M_data =  _M_data->_M_parent;
 		return *this;
 	}
 
-	iterator_list operator--(int)
+	iterator_list<NodeT> operator--(int)
 	{
 		auto ret = *this;
 		--*this;
 		return ret;
 	}
 
-	iterator_list& operator++()
+	iterator_list<NodeT>& operator++()
 	{
 		_M_data = _M_data->_M_next;
 		return *this;
 	}
 	
-	iterator_list operator++(int)
+	iterator_list<NodeT> operator++(int)
 	{
 		auto ret = *this;
 		--*this;
 		return ret;
 	}	
 	
-	bool operator!=(const iterator_list& rhs)
+	bool operator!=(const iterator_list<NodeT>& rhs)
 	{
-		return _M_data == rhs._M_data;
+		return _M_data != rhs._M_data;
 	}
 
+	bool operator==(const iterator_list<NodeT>& rhs) const
+	{
+		return (this->_M_data == rhs._M_data);
+	}
+	
 	private:
+
 	NodeT* _M_data;
 };
 
@@ -63,11 +70,24 @@ struct node_list {
 
 	node_list()  = default;
 
-	value_type*  _M_value;
+	value_type  _M_value;
 	node_list<T>* _M_parent;
 	node_list<T>* _M_next;
-	bool _M_nil;
+	bool _M_nil = false;
+
+	template<typename Allocator>
+	static node_list<T>* allocate(Allocator& allocator)
+	{
+		auto node = std::allocator_traits<Allocator>::allocate(allocator, 1);
+		return node;
+	}
 	
+	template<typename Allocator>
+	static void deallocate(Allocator& allocator, node_list<T>* ptr)
+	{
+		std::allocator_traits<Allocator>::destroy(allocator, ptr);
+		std::allocator_traits<Allocator>::deallocate(allocator, ptr, 1);
+	}
 };
 
 /**
@@ -82,33 +102,31 @@ completar todo o processo primeiro terá prioridade, somente neste momento o ite
 valor é checado, fora este cenário itens de mesmo valor podem ser inseridos normalmente.
 
 */
-template<typename T>
+template<typename T, 
+		typename TEqual = std::equal_to<T>, 
+		typename Allocator = std::allocator<T>>
 class list_lock_free {
 public:
+	
 	using node_type = node_list<T>;
-	using node_ptr = node_type*;
+
+	using value_type = T;
+	using allocator_type = Allocator;
+	using alloc_traits = std::allocator_traits<allocator_type>;
+	
+	using pointer = typename alloc_traits::pointer;
+	using const_pointer = typename alloc_traits::const_pointer;
+	using size_type = typename alloc_traits::size_type;
+	using difference_type = typename alloc_traits::difference_type;
+
 	using iterator = iterator_list<node_type>;
+	using node_ptr = node_type*;
+	using allocator_node = typename alloc_traits::template rebind_alloc<node_type>;
 
 	list_lock_free() 
 	{
-	//	_M_end.store(new node_type());
-	//	_M_begin.store(new node_type());
-		//_M_begin->_M_next.store(_M_end);
-		//_M_begin->_M_parent.store(_M_end);
-		//_M_end->_M_parent.store(_M_begin);
-	}
-
-	list_lock_free(list_lock_free<T>&& other) :
-		_M_begin(other._M_begin.load()),
-		_M_end(other._M_end) {
-		other._M_begin = nullptr;
-		other._M_end  = nullptr;
-	}
-	list_lock_free<T>& operator=(const list_lock_free<T>& other)
-	{
-		_M_begin.store(other._M_begin.load());
-		_M_end.store(other._M_end.load());
-		return *this;
+		_M_end.store(node_type::allocate(_M_allocator));
+		_M_begin.store(_M_end.load());
 	}
 
 	iterator begin()
@@ -123,34 +141,67 @@ public:
 	/*
 		
 	*/	
+
+	iterator find(const T& value)
+	{
+		auto ret = begin();
+		auto last = end();
+		while(ret != last && *ret != value) {
+			++ret;
+		}
+		return ret;
+	}
+	template<bool force=true>
 	iterator insert(T value)
 	{
-		node_ptr beg = _M_begin.load();
-		node_ptr new_node = new node_type();	
-		new_node->_M_next = beg;
+		//start point
+		node_ptr orig_head  = _M_begin.load();
+		//step-1 Check if the value already exists in the list;
+		{
+			auto start = orig_head; 
+			auto last = _M_end.load();
+			while(start != last && start->_M_value != value) 
+			{
+				++start;
+			}
+			if(start != last)
+			{
+				//found
+				if(force)
+				{
+					start->_M_value = value;
+				}
+				return iterator(start);
+			}
+		}		
+		//step-2 allocate new node and try insert into begin of the list
+		auto new_node =  node_type::allocate(_M_allocator);	
+		new_node->_M_next = orig_head;
 		while(!_M_begin.compare_exchange_strong(new_node->_M_next, new_node))
 		{
-			//agora verificamos se algum valor igual jã foi inserido
-			//agora new_node->_M_next ~e a atual header 
-			//entao pode existir um range de nós entre _M_next - beg
+			//step-3 this case only occurs when the head was change by another
+			//thread, so we need first update our begin reference,
+			//try find if the new node is equals to the value and try again if necessary
+
 			auto head = new_node->_M_next;
-			while(head != _M_end)
-			{
-				/*
-				if(head->_M_value == value)
-				{
-					return iterator(head);
-				}*/
-			}
-			//o nó que iriamos colocar foi removido
-			//então começamos novamente e sabemos que 
-			//este valor ainda não foi inserido
-			if(head == _M_end)
+			while(head != _M_end && head != orig_head )
 			{
 				
+				if(TEqual{}(head->_M_value, value))
+				{
+					if(force)
+					{
+						head->_M_value = value;
+					}
+					//discart the new node
+					node_type::deallocate(_M_allocator, new_node);
+					return iterator(head);
+				}
+				++head;
 			}
+			orig_head = new_node->_M_next;
 		}
-		return iterator(_M_end.load());
+		return iterator(new_node);
 	}
 
 	iterator try_insert(T value)
@@ -161,6 +212,8 @@ public:
 	
 	iterator erase(T value)
 	{
+		auto head = _M_begin.load();
+
 		return iterator(_M_end.load());
 	} 
 
@@ -170,43 +223,81 @@ public:
 private:
 	std::atomic<node_type*> _M_begin;
 	std::atomic<node_type*> _M_end;
+	allocator_node _M_allocator;
 };
 
-template<class KeyT, class ValueT, class HashFuncT = std::hash<KeyT>>
+template<class KeyT, class ValueT, 
+		class HashFuncT = std::hash<KeyT>, class AllocatorT = std::allocator<std::pair<KeyT, ValueT>>>
 class hash_table_lock_free {
 
 	using value_type = std::pair<KeyT, ValueT>;
-	using iterator = typename list_lock_free<value_type>::iterator;
-	using chain_type = list_lock_free<iterator>;
-	using bulk = chain_type*;
+	using allocator_type = AllocatorT;
+	using alloc_traits = std::allocator_traits<allocator_type>;
 
-	struct hash_table_properties
+	struct equal_value_type{
+		bool operator()(const value_type& lhs, const value_type& rhs)
+		{
+			return lhs.first == rhs.first;
+		}
+
+	};
+
+	using iterator = typename list_lock_free<value_type,
+						equal_value_type,
+						allocator_type>::iterator;
+
+	struct equal_iterator_type {
+		bool operator()(const iterator& lhs, const iterator& rhs)
+		{
+			return lhs == rhs;
+		}
+	};
+	using bucket_type = list_lock_free<iterator, 
+						equal_iterator_type, 
+						allocator_type>;
+	using vec_bucket = bucket_type*;
+
+	struct properties
 	{
+		properties() : _M_size(0x00), _M_count(0x00), _M_lock(false),
+		_M_table(nullptr), _M_hash() {}
+		
 		size_t _M_size;
 		size_t _M_count;
 		std::atomic<bool> _M_lock;
-		bulk _M_table;		
+		vec_bucket _M_table;		
 		HashFuncT _M_hash;
 
-		static hash_table_properties* create_table(size_t size)
+		template<typename AllocatorT1>
+		static properties* allocate(AllocatorT1& allocator)
 		{
-			hash_table_properties* table = new hash_table_properties();
-			initialize_table(*table, size);	
+			auto table = std::allocator_traits<AllocatorT1>::allocate(allocator, 1);
+			::new (static_cast<void *>(table)) properties();
 			return table;
-
 		}
 
-		static void initialize_table(hash_table_properties& table, size_t size)
+		template<typename AllocatorT1>
+		static void initialize_table(AllocatorT1& allocator, properties& table, size_t size)
 		{
-			table._M_table = new  chain_type[size];
+			using allocator_chain = typename std::allocator_traits<AllocatorT1>::template rebind_alloc<bucket_type>;
+			auto chain = std::allocator_traits<allocator_chain>::allocate(allocator, size);
+			table._M_table =  chain;
 			table._M_size = size;
 			table._M_count = 0x00;
 			for(int i = 0; i < table._M_size; ++i)
 			{
-				table._M_table[i] = std::move(chain_type());
+				::new(static_cast<void *>(&(table._M_table[i]))) bucket_type();
 			}	
 		}
+
+		template<typename AllocatorT1>
+		static void deallocate(AllocatorT& allocator, properties* ptr)
+		{
+		};
 	};
+
+	using alloc_properties = typename alloc_traits::template rebind_alloc<properties>;
+	using alloc_bucket = typename alloc_traits::template rebind_alloc<bucket_type>;
 
 
 
@@ -214,8 +305,9 @@ class hash_table_lock_free {
 
 	hash_table_lock_free() 
 	{
+		constexpr unsigned int init_size = (1 << 4);
 
-		_M_table.store(hash_table_properties::create_table(1<<4));
+		_M_table.store(properties::allocate(_M_allocator_table));
 		if(_M_table.is_lock_free())
 		{
 			std::cout << "is lock free" << std::endl;
@@ -226,14 +318,18 @@ class hash_table_lock_free {
 		}
 	}
 
+	~hash_table_lock_free()
+	{
+		std::cout << "";
+	}
+
 	void try_resize()
 	{
 		auto ptr = _M_table.load();
 		bool expected = false;
 		if(ptr->_M_lock.compare_exchange_strong(expected, true))
 		{
-			hash_table_properties* new_table = 	
-				hash_table_properties::create_table(ptr->_M_size << 1);
+			auto new_table = properties::allocate(_M_allocator_table);
 			new_table->_M_lock.store(true);
 			_M_table.store(new_table);
 			/*
@@ -250,6 +346,7 @@ class hash_table_lock_free {
 
 	iterator insert(value_type value)
 	{
+		constexpr float max_load_factor = 0.7F;
 		auto ptr = _M_table.load();
 		auto index = calculate_index(ptr->_M_hash, value.first, ptr->_M_size);
 		iterator it;	
@@ -259,7 +356,7 @@ class hash_table_lock_free {
 		{
 			insert(value);
 		}
-		if(load_factor() >= 0.7f)
+		if(load_factor() >= max_load_factor)
 		{
 			try_resize();	
 		}
@@ -292,6 +389,10 @@ class hash_table_lock_free {
 		return hash(key) % size;
 	};
 
-	std::atomic<hash_table_properties*> _M_table;
+	
+private:	
+	std::atomic<properties*> _M_table;
+	alloc_properties _M_allocator_table;
+	alloc_bucket _M_alloc_bucket;
 };
 #endif
--- tests/HashTableTests.cpp
+++ tests/HashTableTests.cpp
@@ -1,22 +1,12 @@
 
-#include <gtest/gtest.h>
-#include "hash_table.hpp"
+#include "gtest/gtest.h"
+
 #include "hash_table_lock_free.hpp"
 #include <string>
 
 TEST(HashTableT, CreateHash)
 {
-	auto v = xhash_table<std::string, int>();
-	v.insert({"pipoca", 1});
-	v.insert({"arroz", 2});
-	v.insert({"cachorro", 4});
-	v.print_table();
-	v.print_list();
-	v.erase("cachorro");
-	v.print_table();
-	v.print_list();
-
-	auto x = hash_table_lock_free<std::string, int>();
+	hash_table_lock_free<std::string, int>x;
 	x.insert({"pipoca", 3});
 }
 
